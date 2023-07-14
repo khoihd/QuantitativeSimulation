ba3fcde6c4e8b38a7b6c3da7129f78d30f55cc94
Khoi Hoang <khoi.hoang@wustl.edu>
2023-07-14 14:52:55 -0500

    Add Scipy simulation of Poisson

---
+++ discrete/poisson.py
@@ -0,0 +1,43 @@
+import numpy as np
+import time
+from scipy.stats import poisson
+
+N = pow(10, 8)
+lambda_par = 10
+expected_value = lambda_par
+variance = lambda_par
+
+
+# TODO:
+def simulation():
+    None
+
+
+# TODO:
+def batch_simulation():
+    None
+
+
+def scipy_simulation():
+    start_time = time.time()
+    poisson_prob_dist = poisson(lambda_par)
+    samples = poisson_prob_dist.rvs(N)
+    sample_squares = np.square(samples)
+
+    sum_samples = sum(samples)
+    sum_of_squares = sum(sample_squares)
+
+    print("--------------------------------------------------------")
+    print("Empirical Expected Value: {}".format(sum_samples / N))
+    print("Theoretical Expected Value: {}".format(expected_value))
+
+    print("Empirical Expected Value: {}".format(sum_of_squares / N - (sum_samples / N) ** 2))
+    print("Theoretical Expected Value: {}".format(variance))
+
+    print("Runtime: {} seconds".format(time.time() - start_time))
+
+
+if __name__ == "__main__":
+    simulation()
+    batch_simulation()
+    scipy_simulation()
