09a06b6894d5bdc4fc0861bba5bae3447a35583d
Khoi Hoang <khoi.hoang@wustl.edu>
2023-07-19 15:22:08 -0500

    Add Box-Muller method to generate normal distributions from uniform distributions

--- continuous/normal.py
+++ continuous/normal.py
@@ -1,27 +1,35 @@
 import numpy as np
 import time
-from scipy.stats import norm
+from scipy.stats import norm, uniform
+import matplotlib.pyplot as plt
 
-N = pow(10, 9)
+N = pow(10, 8)
 mu = 0
-sigma = 10
+sigma = 3
 
 expected_value = mu
 variance = sigma**2
 
 
 # X ~ Uniform[0, 100)
-def simulate():
+def box_muller_simulate():
     start_time = time.time()
 
-    samples = []
-    sample_squares = np.square(samples)
+    U = uniform.rvs(0, 1, N)
+    V = uniform.rvs(0, 1, N)
+
+    X = np.sqrt(-2 * np.log(U)) * np.cos(2 * np.pi * V) * sigma + mu
+    Y = np.sqrt(-2 * np.log(U)) * np.sin(2 * np.pi * V) * sigma + mu
+
+    # Test by plotting the probability distribution
+    plt.hist(X, bins=200)
+    plt.show()
 
     print("--------------------------------------------------------")
-    print("Empirical Expected Value: {}".format(np.mean(samples)))
+    print("Empirical Expected Value: {}".format(np.mean(X)))
     print("Theoretical Expected Value: {}".format(expected_value))
 
-    print("Empirical Variance: {}".format(np.mean(sample_squares) - np.mean(samples) ** 2))
+    print("Empirical Variance: {}".format(np.var(X)))
     print("Theoretical Variance: {}".format(variance))
 
     print("Runtime: {} seconds".format(time.time() - start_time))
@@ -31,18 +39,17 @@ def scipy_simulate():
     start_time = time.time()
 
     samples = norm.rvs(loc=mu, scale=sigma, size=N)
-    sample_squares = np.square(samples)
 
     print("--------------------------------------------------------")
     print("Empirical Expected Value: {}".format(np.mean(samples)))
     print("Theoretical Expected Value: {}".format(expected_value))
 
-    print("Empirical Variance: {}".format(np.mean(sample_squares) - np.mean(samples) ** 2))
+    print("Empirical Variance: {}".format(np.var(samples)))
     print("Theoretical Variance: {}".format(variance))
 
     print("Runtime: {} seconds".format(time.time() - start_time))
 
 
 if __name__ == "__main__":
-    # simulate()
-    scipy_simulate()
+    box_muller_simulate()
+    # scipy_simulate()
