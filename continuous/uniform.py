6a06592a3182df6bbc9248e09d5838ca6427dae6
Khoi Hoang <khoi.hoang@wustl.edu>
2023-07-19 15:24:03 -0500

    Use np.var instead of Var[X] = E[X^2] - E^2[X]

--- continuous/uniform.py
+++ continuous/uniform.py
@@ -15,13 +15,12 @@ def simulate():
     start_time = time.time()
 
     samples = np.random.random(N) * (b - a) + a
-    sample_squares = np.square(samples)
 
     print("--------------------------------------------------------")
     print("Empirical Expected Value: {}".format(np.mean(samples)))
     print("Theoretical Expected Value: {}".format(expected_value))
 
-    print("Empirical Variance: {}".format(np.mean(sample_squares) - np.mean(samples) ** 2))
+    print("Empirical Variance: {}".format(np.var(samples)))
     print("Theoretical Variance: {}".format(variance))
 
     print("Runtime: {} seconds".format(time.time() - start_time))
@@ -31,13 +30,12 @@ def scipy_simulate():
     start_time = time.time()
 
     samples = uniform.rvs(loc=a, scale=b-a, size=N)
-    sample_squares = np.square(samples)
 
     print("--------------------------------------------------------")
     print("Empirical Expected Value: {}".format(np.mean(samples)))
     print("Theoretical Expected Value: {}".format(expected_value))
 
-    print("Empirical Variance: {}".format(np.mean(sample_squares) - np.mean(samples) ** 2))
+    print("Empirical Variance: {}".format(np.var(samples)))
     print("Theoretical Variance: {}".format(variance))
 
     print("Runtime: {} seconds".format(time.time() - start_time))
