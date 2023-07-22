1f219d40a55ec2e58b0a5e67e9375f150417935d
Khoi Hoang <khoi.hoang@wustl.edu>
2023-07-22 15:13:31 -0500

    Implement Rejection Sampling for Standard Normal distribution from Uniform distribution

--- continuous/normal.py
+++ continuous/normal.py
@@ -1,17 +1,18 @@
 import numpy as np
 import time
-from scipy.stats import norm, uniform
+from scipy.stats import norm, uniform, bernoulli
 import matplotlib.pyplot as plt
 
-N = pow(10, 8)
+N = pow(10, 4)
 mu = 0
 sigma = 3
 
 expected_value = mu
-variance = sigma**2
+variance = sigma ** 2
 
 
-# X ~ Uniform[0, 100)
+# X = sqrt(-2lnU) * cos(2piV)
+# Y = sqrt(-2lnU) * sin(2piV)
 def box_muller_simulate():
     start_time = time.time()
 
@@ -35,6 +36,37 @@ def box_muller_simulate():
     print("Runtime: {} seconds".format(time.time() - start_time))
 
 
+# Sample standard normal from Uniform(a, b)
+def rejection_sampling():
+    start_time = time.time()
+
+    a, b = -100, 100
+    M = (b - a) / np.sqrt(2 * np.pi)
+    uniform_pdf = lambda z: 1 / (b - a)
+    normal_pdf = lambda t: 1 / np.sqrt(2 * np.pi) * np.exp(-t ** 2 / 2)
+    samples = np.zeros(N)
+    uniform_dist = uniform(a, b - a)
+    for i in range(N):
+        while True:
+            x = uniform_dist.rvs(1)
+            y = bernoulli.rvs(normal_pdf(x) / (M * uniform_pdf(x)))
+            if y:
+                samples[i] = x
+                break
+
+    plt.hist(samples, bins=200)
+    plt.show()
+
+    print("--------------------------------------------------------")
+    print("Empirical Expected Value: {}".format(np.mean(samples)))
+    print("Theoretical Expected Value: {}".format(0))
+
+    print("Empirical Variance: {}".format(np.var(samples)))
+    print("Theoretical Variance: {}".format(1))
+
+    print("Runtime: {} seconds".format(time.time() - start_time))
+
+
 def scipy_simulate():
     start_time = time.time()
 
@@ -51,5 +83,6 @@ def scipy_simulate():
 
 
 if __name__ == "__main__":
-    box_muller_simulate()
+    # box_muller_simulate()
     # scipy_simulate()
+    rejection_sampling()
