f79c6058a38a47fbd96d55875ad199ab71e894c3
Khoi Hoang <khoi.hoang@wustl.edu>
2023-07-26 12:06:22 -0500

    Add simulation of expected_turn_to_get_n_distinct_types

--- toy_problems/expected_turn_to_get_n_distinct_types/main.py
+++ toy_problems/expected_turn_to_get_n_distinct_types/main.py
@@ -1,1 +1,29 @@
+import numpy as np
+import time
 
+N = np.power(10, 6)
+type_count = 2
+
+
+def simulation():
+    start_time = time.time()
+    samples = list(range(type_count))
+
+    expected_turns = 0
+    for _ in np.arange(N):
+        distinct_samples = set()
+        turn = 0
+        while True:
+            sample = np.random.choice(samples)
+            turn += 1
+            distinct_samples.add(sample)
+            if len(distinct_samples) == type_count:
+                expected_turns += turn
+                break
+
+    print("Expected turns = {}".format(expected_turns / N))
+    print("Runtime = {}s".format(time.time() - start_time))
+
+
+if __name__ == "__main__":
+    simulation()
